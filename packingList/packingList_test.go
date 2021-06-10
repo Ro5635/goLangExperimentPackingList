c77172bf534258ed09b4d81dca6c1df6bfb27ffa
robert.curran@clicktravel.com <robert.curran@clicktravel.com>
2021-06-11 00:23:07 +0100

    [NO-JIRA] Added Unit Test For PackingList

---
+++ packingList/packingList_test.go
@@ -0,0 +1,85 @@
+package packingList
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestShouldCalculatePackList(t *testing.T) {
+	t.Run("should CalculatePackList", func(t *testing.T) {
+		// Given
+		packSizes := []int{20, 10, 5, 3, 2}
+		var requestedCount = 44
+
+		// When
+		packingListForRequest, err := GetPackingList(packSizes, requestedCount)
+		if err != nil {
+			t.Fail()
+		}
+
+		// Then
+		expectedPackingList := make(map[int]int)
+		expectedPackingList[20] = 2
+		expectedPackingList[10] = 0
+		expectedPackingList[5] = 0
+		expectedPackingList[3] = 1
+		expectedPackingList[2] = 1
+
+		isExpectedResult := reflect.DeepEqual(packingListForRequest, expectedPackingList)
+		if !isExpectedResult {
+			t.Fail()
+		}
+	},
+	)
+}
+
+func TestShouldCalculatePackListWithSinglePackSize(t *testing.T) {
+	// Given
+	packSizes := []int{1}
+	var requestedCount = 9
+
+	// When
+	packingListForRequest, err := GetPackingList(packSizes, requestedCount)
+	if err != nil {
+		t.Fail()
+	}
+
+	// Then
+	expectedPackingList := make(map[int]int)
+	expectedPackingList[1] = 9
+
+	isExpectedResult := reflect.DeepEqual(packingListForRequest, expectedPackingList)
+	if !isExpectedResult {
+		t.Fail()
+	}
+}
+
+func TestShouldReturnErrorWithInvalidRequestedCount(t *testing.T) {
+	// Given
+	packSizes := []int{10, 5, 3}
+	var requestedCount = -3
+
+	// When
+	_, err := GetPackingList(packSizes, requestedCount)
+
+	// Then
+	if err == nil {
+		t.Fail()
+	}
+
+}
+
+func TestShouldReturnErrorWithInvalidPackSizes(t *testing.T) {
+	// Given
+	packSizes := []int{0}
+	var requestedCount = 2
+
+	// When
+	_, err := GetPackingList(packSizes, requestedCount)
+
+	// Then
+	if err == nil {
+		t.Fail()
+	}
+
+}
