79df28cb87ab39309747f21818265296ff5e21a7
robert.curran@clicktravel.com <robert.curran@clicktravel.com>
2021-06-11 02:45:08 +0100

    [NO-JIRA] Final Clean Up

--- web_app/README.md
+++ web_app/README.md
@@ -1,3 +1,5 @@
+This is a Create React App starter with all the custom code in `app.js` :) 
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- web_app/src/App.js
+++ web_app/src/App.js
@@ -3,7 +3,7 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import {useState} from "react";
 
-// I don't pretend this is not idiomatic React
+// I don't pretend this is idiomatic React 😉
 
 const packsApiUrl = 'https://packs-example.projects.robertcurran.uk';
 
